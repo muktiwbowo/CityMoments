725bed123306f8740ecc9f7386ca394d9c14d94e
Mukti Wibowo <muktiwbowo@gmail.com>
2025-11-29 23:12:46 +0700

    [build] add google maps dependencies

--- app/src/main/java/com/svault/citymoments/MainActivity.kt
+++ app/src/main/java/com/svault/citymoments/MainActivity.kt
@@ -6,12 +6,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -19,13 +17,15 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.svault.citymoments.ui.theme.CityMomentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -69,12 +69,18 @@ fun MainScreen() {
             }
         }
     ) { innerPadding ->
-        Column(
-            Modifier
-                .padding(innerPadding)
+        val yogyakarta = LatLng(-7.7956, 110.3695)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(yogyakarta, 12f)
+        }
+
+        GoogleMap(
+            modifier = Modifier
                 .fillMaxSize()
+                .padding(innerPadding),
+            cameraPositionState = cameraPositionState
         ) {
-            Text(text = "Map View")
+
         }
     }
 }
